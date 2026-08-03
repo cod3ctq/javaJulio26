f11c8e46d0b83ad2d50d4e22bdb3523f666d1b2d
Danielcastillo45 <dany.l.castillo45@gmail.com>
2026-08-03 10:57:22 -0600

    colecciones S2

---
+++ colecciones_delc/src/Auto.java
@@ -0,0 +1,81 @@
+public class Auto {
+
+    private String modelo;
+    private String color;
+    private double precio;
+    private int anio;
+    private String transmisipn;
+    private String vin;
+
+
+    public Auto(){}
+
+    public Auto(String modelo, String color, double precio, int anio, String transmisipn, String vin) {
+        this.modelo = modelo;
+        this.color = color;
+        this.precio = precio;
+        this.anio = anio;
+        this.transmisipn = transmisipn;
+        this.vin = vin;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
+    public String getTransmisipn() {
+        return transmisipn;
+    }
+
+    public void setTransmisipn(String transmisipn) {
+        this.transmisipn = transmisipn;
+    }
+
+    public String getVin() {
+        return vin;
+    }
+
+    public void setVin(String vin) {
+        this.vin = vin;
+    }
+
+    @Override
+    public String toString() {
+        return "Auto{" +
+                "modelo='" + modelo + '\'' +
+                ", color='" + color + '\'' +
+                ", precio=" + precio +
+                ", anio=" + anio +
+                ", transmisipn='" + transmisipn + '\'' +
+                ", vin='" + vin + '\'' +
+                '}';
+    }
+}
